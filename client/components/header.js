4a2842fcaeef5da9f2d17fc025c0513cc99498c6
Ingo Fahrentholz <if@Ingos-MBP.fritz.box>
2017-12-31 02:06:18 +0100

    setup bootstrap and basic header component + form

---
+++ client/components/header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+const Header = () => {
+  return (
+    <nav className="nav navbar-default">
+      <div className="navbar-header">
+        <a href="#" className="navbar-brand">Meteor-Bity</a>
+      </div>
+    </nav>
+  )
+}
+
+
+export default Header;
---
+++ client/main.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Header from './components/header';
+import LinkCreate from './components/link-create';
+
+
+const App = () =>  {
+  return (
+    <div>
+      <Header />
+      <LinkCreate />
+    </div>
+  )
+}
+
+Meteor.startup(() => {
+  ReactDOM.render(<App />, document.querySelector('.render-target'));
+});
+
